6479d6fa9dab4e16e392ee06fbe653362e0bac81
Deepak Dutta <coder.deepakyt@gmail.com>
2023-01-18 09:24:32 -0700

    Implemented skeleton loading animation while retrieving data from database

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
-import { Grid, HStack } from "@chakra-ui/react";
-import React from "react";
+import { Grid, HStack, Progress, Skeleton } from "@chakra-ui/react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Base from "../../components/navbar";
@@ -9,9 +9,13 @@ import { getallproducts } from "./helper";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
   const { products, rerender } = useSelector((state) => state.PRODUCT);
   useEffect(() => {
+    setLoading(true);
     getallproducts().then((response) => {
+      setLoading(false);
       if (!response.data) {
         return toast(response.error.message || "Something went wrong", {
           type: "error",
@@ -23,8 +27,24 @@ const Home = () => {
       }
     });
   }, [rerender]);
+
+  // console.log(loading);
+  {
+    Array(9).fill(" ");
+  }
+
   return (
     <Base>
+      {loading && (
+        <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={6}>
+          {Array(9)
+            .fill(" ")
+            .map((_, index) => {
+              return <Skeleton key={index} height={"380px"} />;
+            })}
+        </Grid>
+      )}
+
       <Grid templateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={6}>
         {products.map((product, index) => {
           return <ProductCard key={index} product={product} />;
